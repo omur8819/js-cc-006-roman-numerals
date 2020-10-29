d52754d7ca14e9b14adb1e153ea1213e40f5dc2f
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-29 19:21:39 +0300

    pyhton roman numerals code added

---
+++ roman_numerals.py
@@ -0,0 +1,16 @@
+num_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+
+
+def convert_roman(num):
+
+    roman = ''
+
+    while num > 0:
+        for i, r in num_map:
+            while num >= i:
+                roman += r
+                num -= i
+
+    return roman
+    
+print(convert_roman(2850))
