6062c6daaee6391fc1784d57ab8287f3f115033c
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-29 19:20:22 +0300

    project-update

---
+++ script.js
@@ -0,0 +1,42 @@
+const checkButton = document.querySelector("#checkButton");
+checkButton.addEventListener("click", check);
+
+const num = document.getElementById("numberPreCheck");
+
+const result = document.getElementById("result");
+
+
+function check() {
+    decimalRoman(num.value)
+}
+
+ 
+// roman numeral
+var romanNum = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+
+// decimal number
+var dNum = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+
+function decimalRoman(value) {
+    
+    if (value <= 0 || value >= 3000)
+       return alert("Please enter any number between 0-3000");
+       var romanNumeral = "";
+
+    for (var i = 0; i<romanNum.length; i++) {
+       while (value >= dNum[i]) {
+          value -= dNum[i];
+          romanNumeral += romanNum[i];
+       }
+    }
+    result.innerHTML = `${num.value} converted as ${romanNumeral}`;
+    num.value = "";
+    
+}
+
+numberPreCheck.addEventListener("keyup", enter);
+function enter(event) {
+    if (event.keyCode===13){
+        check();
+    }
+};
