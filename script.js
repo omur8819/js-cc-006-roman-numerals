2fbf8b03f67ef82f19d97e871a2983cdec75a3c8
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-31 23:27:01 +0300

    project-update

--- script.js
+++ script.js
@@ -20,13 +20,13 @@ var dNum = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
 function decimalRoman(value) {
     
     if (value <= 0 || value >= 3000)
-        return alert("Please enter any number between 0-3000");
-        var romanNumeral = "";
+       return alert("Please enter any number between 0-3000");
+       var romanNumeral = "";
 
     for (var i = 0; i<romanNum.length; i++) {
-        while (value >= dNum[i]) {
-        value -= dNum[i];
-        romanNumeral += romanNum[i];
+       while (value >= dNum[i]) {
+          value -= dNum[i];
+          romanNumeral += romanNum[i];
        }
     }
     result.innerHTML = `${num.value} converted as ${romanNumeral}`;
@@ -40,9 +40,3 @@ function enter(event) {
         check();
     }
 };
-
-
-function findBigger(value){
-    
-}
-
--- script2.js
+++ script2.js
@@ -1,155 +1,26 @@
-var RomanNumbers = function() {
+const map = new Map();
+map.set('M', 1000);
+map.set('CM', 900);
+map.set('D', 500);
+map.set('CD', 400);
+map.set('C', 100);
+map.set('XC', 90);
+map.set('L', 50);
+map.set('XL', 40);
+map.set('X', 10);
+map.set('IX', 9);
+map.set('V', 5);
+map.set('IV', 4);
+map.set('I', 1);
 
-	var _letters = ["I", "V", "X", "L", "C", "D", "M", "(V)", "(X)", "(L)", "(C)", "(D)", "(M)"];
-	var _regexp = /^((\(M\)){0,3})(\(C\)\(M\)|\(C\)\(D\)|(\(D\))?(\(C\)){0,3})(\(X\)\(C\)|\(X\)\(L\)|(\(L\))?(\(X\)){0,3})(M\(X\)|M\(V\)|(\(V\))?)(M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/;
+const convertToRoman = (number) => {
+  if (number === 0) {
+    return '';
+  }
 
-	//---Method to test a Roman numeral
-	this.testRoman = function(roman) {
-
-		return _regexp.test(roman);
-
-	}
-
-	//---Method to obtain a Roman numeral
-	this.getRomanNumber = function(number) {
-
-		if (number > 3999999) throw "Numbers higher than 3999999 can't be converted to Roman. Try a lower value!";
-
-		var roman = "";
-		var cant = String(number).length;
-		var narray = String(number).split("").reverse();
-
-		var parser = function(item, index, a) {
-            
-			switch(item) {
-
-				case "0":
-				case "1":
-				case "2":
-				case "3":
-					roman = (new Array(Number(item) + 1).join(_letters[index * 2])) + roman;
-				break;
-
-				case "4":
-					roman = _letters[index * 2] + _letters[index * 2 + 1] + roman;
-				break;
-
-				case "5":
-				case "6":
-				case "7":
-				case "8":
-					roman = _letters[index * 2 + 1] + (new Array(Number(item) - 4).join(_letters[index * 2])) + roman;
-				break;
-
-				case "9":
-					roman = _letters[index * 2] + _letters[index * 2 + 2] + roman;
-				break;
-
-			}
-
-		}
-
-		narray.forEach(parser);
-
-		return roman;
-
-	}
-
-	//---Method to obtain an Arabic number
-	this.getArabicNumber = function (roman) {
-
-		if (!this.testRoman(roman)) throw "You have entered an invalid Roman numeral. Please try with another value";
-
-		var reg = /(\()(\w)(\))/g;
-		var simple = "";
-		var values = 0;
-		var array = _regexp.exec(roman);
-
-		array.splice(0, 1);
-		array.splice(1, 1);
-		array.splice(2, 2);
-		array.splice(3, 2);
-		array.splice(4, 1);
-
-		var parser = function(item, index, a) {
-
-			switch(index){
-
-				case 0:
-				case 1:
-				case 2:
-					simple = item.replace(reg, "$2");
-					values += getValue(simple) * 1000;
-				break;
-
-				case 3:
-					simple = item.replace(reg, "$2");
-					values += ((simple.slice(0, 1) == "M") ? getValue(simple.slice(1, 2)) * 1000 - getValue(simple.slice(0, 1)) : getValue(simple) * 1000);
-				break;
-
-				case 4:
-				case 5:
-				case 6:
-				case 7:
-					values += getValue(item);
-				break;
-
-			}
-
-		}
-
-		array.forEach(parser);
-
-		return values;
-
-	}
-
-	//---Private function to return a number from a Roman numeral string
-	function getValue(str) {
-
-		var cant = str.length;
-		var chars = [];
-		var ret = 0;
-
-		switch(cant) {
-
-			case 1:
-				ret = getNumberByIndex(_letters.indexOf(str));
-			break;
-
-			case 2:
-				chars = str.split("");
-				ret = ((_letters.indexOf(chars[0]) < _letters.indexOf(chars[1])) ? getNumberByIndex(_letters.indexOf(chars[1])) - getNumberByIndex(_letters.indexOf(chars[0])) : getNumberByIndex(_letters.indexOf(chars[0])) + getNumberByIndex(_letters.indexOf(chars[1])));
-			break;
-
-			case 3:
-				chars = str.split("");
-				ret = sumAllNumbers(getNumberByIndex(_letters.indexOf(chars[0])), getNumberByIndex(_letters.indexOf(chars[1])), getNumberByIndex(_letters.indexOf(chars[2])));
-			break;
-
-			case 4:
-				chars = str.split("");
-				ret = sumAllNumbers(getNumberByIndex(_letters.indexOf(chars[0])), getNumberByIndex(_letters.indexOf(chars[1])), getNumberByIndex(_letters.indexOf(chars[2])), getNumberByIndex(_letters.indexOf(chars[3])));
-			break;
-
-		}
-
-		return ret;
-
-	}
-
-	//---Private function to return a number taking into account the index in the letter Array
-	function getNumberByIndex(index) {
-		return ((index % 2 == 0) ? Math.pow(10, index / 2) : Math.pow(10, (index + 1) / 2) / 2);
-	}
-
-	//---Private function to sum multiple numbers
-	function sumAllNumbers() {
-
-		return Array.prototype.reduce.call(arguments, function (s, n) {
-			return s + n;
-		}, 0);
-
-	}
-
-}
+  for (const [roman, integer] of map.entries()) {
+    if (number >= integer) {
+      return roman + convertToRoman(number - integer);
+    }
+  }
+};
